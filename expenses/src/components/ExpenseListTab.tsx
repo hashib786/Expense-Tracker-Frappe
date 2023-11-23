8244b4a8c403bed9ca19bbe20154a1b218952b56
Hashib Raja <hashibraja23@gmail.com>
2023-11-23 05:54:33 +0000

    feat : Added custome modal and showing after clicking add ➕➕

--- expenses/src/components/ExpenseListTab.tsx
+++ expenses/src/components/ExpenseListTab.tsx
@@ -1,19 +1,23 @@
-import { Box, Button, Center, HStack, Heading, Spinner, Stack } from "@chakra-ui/react"
+import { Box, Button, Center, HStack, Heading, Spinner, Stack, useDisclosure } from "@chakra-ui/react"
 import { useFrappeGetDocList } from "frappe-react-sdk"
 import { ExpenseRecord } from "../types/ExpenseTracker/ExpenseRecord"
 import ExpenseTable from "./ExpenseTable"
 import Error from "./Error"
+import Modals from "./Modals"
+
 
 const ExpenseListTab = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
     const { data, isLoading, error } = useFrappeGetDocList<ExpenseRecord>("Expense Record", { fields: ["*"] })
     console.log({ data, isLoading, error }, JSON.stringify(data))
     return (
         <Stack>
             {error && <Error messege={error.message} />}
+            <Modals isOpen={isOpen} onClose={onClose} tittle="Tittle" description="Description" />
             <HStack justify="space-between">
                 <Heading as="h3" fontSize="x-large">Expenses</Heading>
                 <Box>
-                    <Button colorScheme="blue" >Add</Button>
+                    <Button colorScheme="blue" onClick={onOpen}>Add</Button>
                 </Box>
             </HStack>
             {isLoading && <Center height="40vh"><Spinner /></Center>}
---
+++ expenses/src/components/Modals.tsx
@@ -0,0 +1,31 @@
+import { Button, ModalBody, Modal, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react"
+import { ReactNode } from "react";
+
+type Props = {
+    isOpen: boolean;
+    onClose: () => void;
+    tittle: string;
+    description: string | ReactNode;
+}
+const Modals = ({ isOpen, onClose, tittle, description }: Props) => {
+    return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>{tittle}</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    {description}
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button colorScheme='blue' mr={3} onClick={onClose}>
+                        Close
+                    </Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
+}
+
+export default Modals
